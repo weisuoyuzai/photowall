2be6c10b439a6b1ba10ae76c9b7bbefd25d9bd0a
xiaoyuyu <administrator@forgiveyu.com>
2018-01-09 20:21:45 +0800

    加入vue-waterfall实现瀑布流

--- src/base/image/image.vue
+++ src/base/image/image.vue
@@ -1,20 +1,50 @@
 <template>
     <div>
-      <scroll :data="imgData" class="image-wrapper">
+      <scroll :data="imgData" class="image-wrapper" v-if="imgData">
+        <!--<div class="wrapper">-->
+          <!--<div v-for="item in imgData">-->
+            <!--<img :src="'https://api.forgiveyu.com/'+item.path" alt="">-->
+          <!--</div>-->
+        <!--</div>-->
         <div class="wrapper">
-          <div v-for="item in imgData">
-            <img :src="'https://api.forgiveyu.com/'+item.path" alt="">
-          </div>
+          <waterfall :line-gap="getRealHalfWidth" auto-resize="true">
+            <waterfall-slot v-for="item in imgData" :width="getHalfWidth" :height="resetHeight(item.height)">
+              <div class="item">
+                <img :src="'https://api.forgiveyu.com/'+item.path" alt="" class="image-item">
+              </div>
+            </waterfall-slot>
+          </waterfall>
         </div>
       </scroll>
+      <loading class="loading" v-show="!imgData"></loading>
     </div>
 </template>
 
 <script>
-    import scroll from 'base/scroll/scroll'
+    import scroll from 'base/scroll/scroll';
+    import loading from 'base/loading/loading';
+    import {waterfall,waterfallSlot} from 'vue-waterfall';
     export default {
         components:{
-          scroll
+          scroll,
+          loading,
+          waterfall,
+          waterfallSlot
+        },
+        computed:{
+          getHalfWidth(){
+            return (document.documentElement.clientWidth-10)/2;
+          },
+          getRealHalfWidth(){
+            return (document.documentElement.clientWidth)/2
+          }
+        },
+        methods:{
+          resetHeight(height){
+            let x=(document.documentElement.clientWidth-10)/2/200;
+            return height*x;
+          },
+
         },
         props:{
           imgData:{
@@ -29,4 +59,17 @@
   .image-wrapper
     width 100%
     height: 100% !important
+    .item{
+      padding 5px
+      .image-item{
+        width 100%
+      }
+    }
+
+  .loading
+    position: fixed;
+    top 50%
+    left:50%
+    margin-top -50px
+    margin-left -50px
 </style>
---
+++ src/base/loading/loading.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="loading-wrapper">
+      <img src="./loading.gif" alt="">
+      <p>加载中</p>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped lang="stylus">
+  .loading-wrapper
+    width 70px;
+    height 70px;
+    background rgba(0,0,0,.7);
+    display: flex
+    flex-direction column
+    align-items center
+    color: white
+    padding 15px
+    border-radius 15px
+    p
+      margin-bottom 0
+    img
+      margin-top:10px
+</style>
